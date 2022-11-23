54e1f08faaa0944fea6ebe74140b2b609e5b95e1
marw19 <yakoubi.marwen@esprit.tn>
2022-11-23 09:28:36 +0100

    first commit

---
+++ src/main/java/tn/test/marwenyakoubi/MarwenYakoubiApplication.java
@@ -0,0 +1,13 @@
+package tn.test.marwenyakoubi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarwenYakoubiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarwenYakoubiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/tn/test/marwenyakoubi/entities/Etudiant.java
@@ -0,0 +1,28 @@
+package tn.test.marwenyakoubi.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name="Etudiant")
+//c'est une annotation  lombok qui crée constructerur, getter , setter, hash, to string... !!!!! non recommandé parcequ'on a des associations
+//@Data
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Etudiant implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="idEtudiant")
+    private Long idEtudiant;
+    private String prenomE;
+    private String nomE;
+  
+}
